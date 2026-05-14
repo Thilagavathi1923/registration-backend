7d6e00740ab24a0acc5d0c5b0b882daac55eae1d
Thilak <ncas2527psd2285@gmail.com>
2026-05-14 17:53:49 +0530

    fix railway database connection

--- db.js
+++ db.js
@@ -3,17 +3,16 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "registration_db",
-    port: 3306
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 connection.connect((err) => {
     if (err) {
-        console.log("Database connection failed:");
-        console.log(err);
+        console.log("Database connection failed:", err);
     } else {
         console.log("Connected to MySQL");
     }
